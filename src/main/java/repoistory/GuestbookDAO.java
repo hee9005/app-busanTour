940da062b51c2566479632939658fe40336f81ef
lynx741 <e900590@gmail.comgit config --global user.email hee900590@gmail.comgit config --global user.email e900590@gmail.com>
2023-04-27 11:10:56 +0900

    방명록

---
+++ src/main/java/repoistory/GuestbookDAO.java
@@ -0,0 +1,27 @@
+package repoistory;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+
+
+
+public class GuestbookDAO extends DAO {
+public static int creategusetBook(String target, String content, String boardPass, String writer) {
+		
+		SqlSession session = factory.openSession(true);
+		Map<String, Object> obj = new HashMap<>();
+		obj.put("target", target);
+		obj.put("content", content);
+		obj.put("boardPass", boardPass);
+		obj.put("writer", writer);
+		int r = session.insert("messages.create",obj);
+		System.out.println("r===" +r);
+		session.close();
+		return r;
+	}
+}
