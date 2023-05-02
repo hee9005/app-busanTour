464d546eff2ef1e529f4cdfbd922579809275f05
lynx741 <e900590@gmail.comgit config --global user.email hee900590@gmail.comgit config --global user.email e900590@gmail.com>
2023-05-02 14:32:00 +0900

    명소상세페이지완료

--- src/main/java/controller/AttractionTaskController.java
+++ src/main/java/controller/AttractionTaskController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,22 +11,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import data.Attraction.attractionitem;
+
 import util.AttractionDetailAPI;
 
 @WebServlet("/attraction-task")
 public class AttractionTaskController extends HttpServlet{
+	@SuppressWarnings("unused")
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String no = req.getParameter("no");
-		
-		attractionitem item = AttractionDetailAPI.findByDesertionNO(no);
-		if(item == null) {
-			req.getRequestDispatcher("/WEB-INF/views/not-found.jsp").forward(req, resp);	
-	}else {
-		req.setAttribute("item", item);
-		req.getRequestDispatcher("/WEB-INF/views/attractionDetail.jsp").forward(req, resp);
+		List<attractionitem> attractionitem = new LinkedList<>();
+		String UC_SEQ = req.getParameter("no");
+		attractionitem[] attractionitems = AttractionDetailAPI.getAttractions(UC_SEQ).getItem();
+		for (int i = 0; i < attractionitems.length; i++) {
+		    attractionitem.add(attractionitems[i]);
+		}
+		if (attractionitem.isEmpty()) {
+		    req.getRequestDispatcher("/WEB-INF/views/not-found.jsp").forward(req, resp);
+		} else {
+		    req.setAttribute("item", attractionitem.get(0));
+		    req.getRequestDispatcher("/WEB-INF/views/attractionDetail.jsp").forward(req, resp);
+		}
 	}
-
-}
 }
