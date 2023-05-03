c2252ea8b0e44ce92345a4e838d2735fe45545c5
coco <coco@gmail.com>
2023-05-03 14:01:22 +0900

    축제 페이징 처리

--- src/main/java/controller/FestivalController.java
+++ src/main/java/controller/FestivalController.java
@@ -12,17 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import data.Festival.festivalitem;
 import util.festivalAPI;
-
+import util.festivalAPI2;
+import util.foodAPI2;
 
 @WebServlet("/festival")
-public class FestivalController extends HttpServlet{
+public class FestivalController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		int p;
+		if (req.getParameter("page") == null) {
+			p = 1;
+		} else {
+			p = Integer.parseInt(req.getParameter("page"));
+		}
+
 		String pageNo;
-		if(req.getParameter("pageNo") ==null) {
-			pageNo ="1";
-		}else {
-			pageNo=req.getParameter("pageNo");
+		if (req.getParameter("pageNo") == null) {
+			pageNo = "1";
+		} else {
+			pageNo = req.getParameter("page");
 		}
 		festivalitem[] festivalitems = festivalAPI.getFestivals(pageNo).getItem();
 		List<festivalitem> fes = new LinkedList<>();
@@ -33,9 +42,26 @@ public class FestivalController extends HttpServlet{
 		if (festivalitems != null) {
 			req.setAttribute("festivals", fes);
 		}
-		
+
+		int total = festivalAPI2.getFestivals(pageNo).getTotalCount();
+
+		int lastPage = total / 10 + (total % 10 > 0 ? 1 : 0);
+
+		int last = (int) Math.ceil(p / 5.0) * 5;
+		int start = last - 4;
+
+		last = last > lastPage ? lastPage : last;
+
+		req.setAttribute("start", start); // (1~5) ==> 1 / (6~10) ==>6 / (11~15) ==> 11
+		req.setAttribute("last", last); // (1~5) ==> 5 / (6~10) ==>10 / (11~15) ==> 15
+
+		boolean existPrev = p >= 6;
+		boolean existNext = lastPage > last;
+		req.setAttribute("existPrev", existPrev);
+		req.setAttribute("existNext", existNext);
+
 		req.getRequestDispatcher("/WEB-INF/index/festivalScreen.jsp").forward(req, resp);
-		
+
 	}
 
 }
