ce39864271597e466347cc449040c0883559f4c0
lynx741 <e900590@gmail.comgit config --global user.email hee900590@gmail.comgit config --global user.email e900590@gmail.com>
2023-05-08 16:02:02 +0900

    목록오류 수정

--- src/main/java/guestbookController/listController.java
+++ src/main/java/guestbookController/listController.java
@@ -1,6 +1,9 @@
 package guestbookController;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,22 +27,36 @@ public class listController extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		SqlSessionFactory factory = (SqlSessionFactory) req.getServletContext().getAttribute("sqlSessionFactory");
-		SqlSession sqlSession = factory.openSession();
-		users logonUser = (users) req.getSession().getAttribute("logonUser");
-		
-		Map<String, Object> li = new HashMap<>();
-		String content = req.getParameter("content");
-		String writer = req.getParameter("writer");
-		String writed = req.getParameter("writed");
-		li.put("content", content);
-		li.put("writer", writer);
-		li.put("writed", writed);
-		List<guestBook> list = sqlSession.selectList("messages.findAllDesc", li);
-		req.setAttribute("list", list);
-		
-		
-		req.getRequestDispatcher("/WEB-INF/guestbook/list.jsp").forward(req, resp);
+	    SqlSessionFactory factory = (SqlSessionFactory) req.getServletContext().getAttribute("sqlSessionFactory");
+	    SqlSession sqlSession = factory.openSession();
+	    users logonUser = (users) req.getSession().getAttribute("logonUser");
+
+	    Map<String, Object> li = new HashMap<>();
+	    String content = req.getParameter("content");
+	    String writer = req.getParameter("writer");
+	    String writedParam = req.getParameter("writed");
+	    Date writed = null;
+	    if (writedParam != null && !writedParam.isEmpty()) {
+	        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	        try {
+	            writed = sdf.parse(writedParam);
+	        } catch (ParseException e) {
+	            throw new ServletException("Failed to parse date", e);
+	        }
+	    }
+	    li.put("content", content);
+	    li.put("writer", writer);
+	    li.put("writed", writed);
+
+	    try {
+	        List<guestBook> list = sqlSession.selectList("messages.findAllDesc", li);
+	        req.setAttribute("list", list);
+	    } catch (Exception e) {
+	        throw new ServletException("Failed to retrieve messages", e);
+	    } finally {
+	        sqlSession.close();
+	    }
+
+	    req.getRequestDispatcher("/WEB-INF/guestbook/list.jsp").forward(req, resp);
 	}
-	
 }
